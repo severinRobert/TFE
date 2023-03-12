d8475040a443c62648daf2346f4f2d6a2cd5e012
severinRobert <sevrobe21@gmail.com>
2023-03-12 20:15:41 +0100

    [ADD] production: docker network & init FastAPI & add parameters to marketease script

--- marketease.py
+++ marketease.py
@@ -5,27 +5,34 @@ import sys
 import time
 from subprocess import DEVNULL, Popen
 
-DIRECTORY = '~/Documents/EPHEC/TFE/TFE/'
+DIRECTORY = '~/Documents/EPHEC/TFE/TFE'
 parser = argparse.ArgumentParser('marketease')
 
 
 if __name__ == '__main__':
     parser.add_argument('-p', '--project', help='Name of project to use with docker-compose.', default='marketease')
-    parser.add_argument('action', choices=['up', 'recreate', 'down', 'logs', 'test'], help='Action to perform on the docker-compose project.')
+    parser.add_argument('action', choices=['up', 'restart', 'recreate', 'build', 'down', 'exec', 'logs', 'test'], help='Action to perform on the docker-compose project.')
+    parser.add_argument('service', help='Service to perform action on.', nargs='*')
 
     args = parser.parse_args()
     arguments = ['docker-compose', '-f', f'{DIRECTORY}/docker-compose.yml', '-p', args.project]
 
     if args.action == 'up':
         arguments.extend(['up', '-d'])
+
     elif args.action == 'recreate':
         arguments.extend(['up', '--force-recreate', '-d'])
-    elif args.action == 'down':
-        arguments.extend(['down'])
+
+    elif args.action in ['down', 'build', 'exec', 'restart']:
+        arguments.extend([args.action])
+
     elif args.action == 'logs':
         arguments.extend(['logs', '-ft'])
+
     elif args.action == 'test':
         print('Test environment not implemented yet.')
         exit()
+
+    arguments.extend([' '.join(args.service) if args.service else ''])
     print(' '.join(arguments))
     sys.exit(os.system(' '.join(arguments)))
