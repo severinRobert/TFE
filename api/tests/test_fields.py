b72bde2678243ee428b376e8c82fbb4705268fd5
severinRobert <sevrobe21@gmail.com>
2023-04-08 17:56:42 +0200

    [TEST] api: test initialisation

--- api/tests/test_fields.py
+++ api/tests/test_fields.py
@@ -1,27 +1,30 @@
-import pytest
 from fastapi import status
-from fastapi.testclient import TestClient
 from schemas import Field
 
+field = Field(id=1, name="Prix", description="Le prix du produit", is_required=True, is_filterable=True, type_id=2, selections_group_id=None)
+field2 = Field(id=2, name="Poids", description="Le poids du produit", is_required=False, is_filterable=False, type_id=4, selections_group_id=None)
+
 class TestRouteFields:
-    async def test_add_field(self, client: TestClient, field2: Field):
-        # Add a field
-        response = await client.post("/fields", json=field2.dict())
-        assert response.status_code == status.HTTP_200_OK
-        assert response.json() == field2.dict()
-
-        # Check if the field we added has been added.
-        # We run the tests against a fresh database every time, so it should contain only our field.
-        response = await client.get(f"/fields/{field2.id}")
-        assert response.status_code == status.HTTP_200_OK
-        assert response.json() == field2.dict()
-
-    async def test_get_field(self, client: TestClient, field: Field):
-        # Also check that the path to the individual resource works
-        response = await client.get(f"/fields/{field.id}")
+    def test_add_field(self, client):
+        # Add a fields
+        response = client.post("/fields", json=field.dict())
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == field.dict()
 
+        response = client.post("/fields", json=field2.dict())
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == field2.dict()
+
+    def test_get_field(self, client):
+        # Also check that the path to the individual resource works
+        response = client.get(f"/fields/{field.id}")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == field.dict()
+
+        response = client.get(f"/fields/{field2.id}")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == field2.dict()
+
         # Get a nonexistant resource should return 404
-        response = await client.get("/fields/666")
+        response = client.get("/fields/666")
         assert response.status_code == status.HTTP_404_NOT_FOUND
