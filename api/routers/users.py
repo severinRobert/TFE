98950cf0cb1dbba524977d607a58b8d120ebd70a
severinRobert <sevrobe21@gmail.com>
2023-04-23 12:24:50 +0200

    [ADD] api: email verification #30

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,13 +1,13 @@
-from datetime import datetime, timedelta
-from typing import Annotated
+from datetime import timedelta
 
 from schemas import User
-from fastapi import HTTPException, status, APIRouter, Response, Depends
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import HTTPException, status, APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from jose import JWTError, jwt
 from pydantic import BaseModel
 from database import get_db
+from auth import create_access_token
 
 
 router = APIRouter(
@@ -15,23 +15,12 @@ router = APIRouter(
     tags=["users"],
 )
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 class Token(BaseModel):
     access_token: str
     token_type: str
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
 
 @router.post("/register", response_model=Token)
 async def register_user(user: User, db: Session = Depends(get_db)):
@@ -48,8 +37,7 @@ async def register_user(user: User, db: Session = Depends(get_db)):
 @router.post("/login", response_model=Token)
 async def login_user(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     """Login a user."""
-    user = User(username=form_data.username, email=form_data.username, password=form_data.password)
-    if not await User.login(user, db):
+    if not await User.login(form_data, db):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, 
             detail="Incorrect username or password",
@@ -57,7 +45,7 @@ async def login_user(form_data: OAuth2PasswordRequestForm = Depends(), db: Sessi
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": form_data.username}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
