f3b889a01b020944bf5db434b9711ff5c8df662d
severinRobert <sevrobe21@gmail.com>
2023-05-06 22:09:06 +0200

    [ADD] web: kind of a dark theme to rest my eyes

---
+++ web/src/components/SwitchTheme.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="switch-theme">
+    <input type="checkbox" @click="toggleTheme"/>
+    <label>Toggle theme</label>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "SwitchTheme",
+    data() {
+        return {
+            theme: "light-theme",
+        };
+    },
+    methods: {
+        toggleTheme() {
+          this.theme = this.theme === "light-theme" ? "dark-theme" : "light-theme";
+          document.documentElement.className = this.theme;
+        },
+    },
+    
+};
+</script>
+
+<style>
+.switch-theme {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 1rem;
+}
+</style>
