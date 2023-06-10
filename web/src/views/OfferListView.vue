8642e5ec94ef7f4fba728d9268c76415f4539949
severinRobert <sevrobe21@gmail.com>
2023-06-10 14:15:31 +0200

    [WIP] web: correct the date in offerList

--- web/src/views/OfferListView.vue
+++ web/src/views/OfferListView.vue
@@ -26,7 +26,7 @@
                         {{ typeof(offer['fields'][field.id])== 'boolean' ? (offer['fields'][field.id] ? $t('main.yes') : $t('main.no')) : offer['fields'][field.id] }}
                     </td>
                     <td>{{ offer.username }}</td>
-                    <td>{{ Date(offer.start_datetime).toLocaleString() }}</td>
+                    <td>{{ offer.start_datetime.toLocaleString() }}</td>
                 </tr>
             </tbody>
         </table>
@@ -79,6 +79,13 @@ export default {
     methods: {
         fetchOffers(id=this.productId) {
             headers().get(`/offers/product/${id}/details`).then((response) => {
+                // THIS TAKES TOO LONG SO THE FILTERS DON'T GET THE OFFERS TO FILTER
+                // convert date strings to Date objects and translate month because in js months start from 0
+                // for(let offer of response.data) {
+                //     let date = offer.start_datetime.split(/[-T:.]/).slice(0,-1)
+                //     date[1] = parseInt(date[1]) - 1;
+                //     offer.start_datetime = new Date(Date.UTC(...date));
+                // }
                 this.offers[`${id}`] = response.data;
                 this.productOffers = response.data;
                 this.filteredOffers = response.data;
